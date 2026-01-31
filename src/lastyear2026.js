6fbea68acb8ccbba642fe7e6d4443dde59ffb49d
Shane Thomson <shanet02@vt.edu>
2026-01-31 14:01:13 -0500

    repainted/restructured homepage

--- src/lastyear2026.js
+++ src/lastyear2026.js
@@ -3,11 +3,13 @@ import './lastyear2026.css'
 function LastYear2026(){
     //embeding last years youtube video for now. probably should link itch page too. Not sure where we want to do that though
     return (
-        <div>
-            <iframe  width="1400" height="758" src="https://www.youtube.com/embed/n05WiAtyBf8?si=jhqgwKr1jnBDg67O" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-            <div id='lastYearText'>
-                <h4 id="lastYear">Last Year</h4>
-                <p id="text">Play the games, watch the results</p>
+        <div id = "lastYearBorder">
+            <div id='internalSpace'>
+                <iframe  width="1400" height="758" src="https://www.youtube.com/embed/n05WiAtyBf8?si=jhqgwKr1jnBDg67O" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <div id='lastYearText'>
+                    <h4 id="lastYear">Last Year</h4>
+                    <p id="text">Play the games, watch the results</p>
+            </div>
             </div>
         </div>
     )
